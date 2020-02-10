594c23aacfe67585814f15400fcedc2adf22d3b1
Helen Kershaw <20047007+hkershaw-brown@users.noreply.github.com>
2020-02-10 09:03:28 -0500

    Fixed throttle example

--- job_arrays/job-array-throttle.sh
+++ job_arrays/job-array-throttle.sh
@@ -7,19 +7,19 @@
 #SBATCH -t 0:10:00
 
 # Provide index values (TASK IDs)
-# 40 array tasks, but only 2 can run simultaneously
-#SBATCH --array=1-40%2
+# 4 array tasks, but only 2 can run simultaneously
+#SBATCH --array=1-4%2
 
 # Use '%A' for array-job ID, '%J' for job ID and '%a' for task ID
-#SBATCH -e maths-%a.err
-#SBATCH -o maths-%a.out
+#SBATCH -e throttle-%a.out
+#SBATCH -o throttle-%a.out
 
 # single core
 #SBATCH -c 1
 
 # Using sleep here to make the job take more time so you can 
 # see the effect of throttling. 
-sleep(2)
+sleep 2
 
 # Use the $SLURM_ARRAY_TASK_ID variable to provide different inputs for each job
 input=$((SLURM_ARRAY_TASK_ID*1000+2))
