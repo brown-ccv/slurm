7e49481c61f09d4116ae8c70c4a659288dd7bf36
Paul Hall <paul_hall@brown.edu>
2022-11-07 14:15:38 -0500

    Updated SBATCH commands

--- job_arrays/env-slurm.sh
+++ job_arrays/env-slurm.sh
@@ -1,8 +1,15 @@
 #!/bin/bash
 
-#SBATCH -n 1 
-#SBATCH -t 00:05:00
-#SBATCH -o slurm_environment_variables
+# Resource request
+#SBATCH --time=00:05:00
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=1
+#SBATCH --mem-per-cpu=4G
+
+# Job handling
+#SBATCH -J slurm_env
+#SBATCH -o %x.out
 
 # Slurm sets a bunch of environment variales
 # This script lists all the environment variables
