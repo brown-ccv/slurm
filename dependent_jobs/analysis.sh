ad7c6be28f23e735a836c4b95d4c5a7159210d74
Paul Hall <paul_hall@brown.edu>
2022-11-07 14:14:18 -0500

    Updated SBATCH commands

--- dependent_jobs/analysis.sh
+++ dependent_jobs/analysis.sh
@@ -1,11 +1,16 @@
 #!/bin/bash
 
+# Resource request
 #SBATCH --time=00:05:00
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=1
+#SBATCH --mem-per-cpu=4G
+
+# Job handling
 #SBATCH -J analysis
 #SBATCH -o %x-%j.out
 #SBATCH -e %x-%j.err
-#SBATCH --mem=1G
-#SBATCH -n 1
 
 # Run a command
 echo "Lots of lovely science"
