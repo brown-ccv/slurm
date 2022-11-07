acc6adb6d1ca351b98c92cc644a0758b086f6304
Paul Hall <paul_hall@brown.edu>
2022-11-07 14:16:18 -0500

    Updated SBATCH commands

--- job_arrays/job-array-variable.sh
+++ job_arrays/job-array-variable.sh
@@ -1,9 +1,18 @@
 #!/bin/bash
 
-#SBATCH --time=00:10:00
-#SBATCH --mem=20G
-#SBATCH -c 8
+# Resource request
+#SBATCH --time=00:05:00
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=1
+#SBATCH --mem-per-cpu=4G
+
+# Define array (TASK IDs)
 #SBATCH --array=0-4
+
+# Job handling
+#SBATCH -J arrayjob-variable
+#SBATCH -o %x-%A_%a.out
 
 input=(A B C D E)
 
