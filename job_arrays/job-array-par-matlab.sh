02984fbd4e0158560ef622ba358988490a8a07d8
Paul Hall <paul_hall@brown.edu>
2023-12-04 13:27:52 -0500

    Updated resource allocation syntax

--- job_arrays/job-array-par-matlab.sh
+++ job_arrays/job-array-par-matlab.sh
@@ -1,20 +1,19 @@
 #!/bin/bash
 
-# Job Name
-#SBATCH -J arrayjob 
-
-# Walltime requested
-#SBATCH -t 0:10:00
+# Resource request
+#SBATCH --time=00:05:00
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=1
+# note: MATLAB parpool job, use >1 cpus-per-task
+#SBATCH --cpus-per-task=2
+#SBATCH --mem-per-cpu=4G
 
 # Provide index values (TASK IDs)
 #SBATCH --array=1-4
 
-# Use '%A' for array-job ID, '%J' for job ID and '%a' for task ID
-#SBATCH -e arrayjob-%a.err
-#SBATCH -o arrayjob-%a.out
-
-# two core Matlab parpool job
-#SBATCH -c 2
+# Job handling
+#SBATCH -J arrayjob-matlab
+#SBATCH -o %x-%A_%a.out
 
 # Use the $SLURM_ARRAY_TASK_ID variable to provide different inputs for each job
 
@@ -22,8 +21,6 @@
 # is ~/.matlab/
 # When submiting multiple jobs with  PCT such as when using job arrays, you need to
 # create a unique temporary directory for each job
-
-
 tmpdir=~/matlab_temp_dir/$SLURM_ARRAY_TASK_ID
 
 mkdir -p $tmpdir
